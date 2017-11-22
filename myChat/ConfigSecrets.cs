308746e01b6a7d7c50a551d9ee77663c7c37577c
Adrian Sypos <adrian00935@gmail.com>
2017-11-22 23:21:52 +0000

    Secret config file

---
+++ myChat/ConfigSecrets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myChat
+{
+    static class ConfigSecrets
+    {
+        // Once configured the required Azure services set to true (debugging)
+        public const bool ISAZURECONFIGDONE = true;
+
+        // Azure Mobile Services secrets
+        public const string AzureMobileServicesURI = "YOUR MOBILE SERVICES URI";
+
+        // Azure Notification Hub secrets
+        public const string AzureNotificationHubName = "AZURE NOTIFICATION HUB NAME";
+        public const string AzureNotificationHubCnxString = "XXXXXX-AZURE NOTIFICATION HUB STRING-XXXXXX"
+        // Details for support
+        public const string DeveloperSupportEmail = "g00309646@gmit.ie";
+    }
+}
