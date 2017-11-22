beafe563d9e0e0379f45397204f4e20c1b872819
Adrian Sypos <adrian00935@gmail.com>
2017-11-22 23:22:29 +0000

    Updated gitignore

--- myChat/ConfigSecrets.cs
+++ myChat/ConfigSecrets.cs
@@ -10,11 +10,12 @@ namespace myChat
         public const bool ISAZURECONFIGDONE = true;
 
         // Azure Mobile Services secrets
-        public const string AzureMobileServicesURI = "YOUR MOBILE SERVICES URI";
+        public const string AzureMobileServicesURI = "https://g00309646.azurewebsites.net";
 
         // Azure Notification Hub secrets
-        public const string AzureNotificationHubName = "AZURE NOTIFICATION HUB NAME";
-        public const string AzureNotificationHubCnxString = "XXXXXX-AZURE NOTIFICATION HUB STRING-XXXXXX"
+        public const string AzureNotificationHubName = "g00309646";
+        public const string AzureNotificationHubCnxString = "Endpoint=sb://g00309646.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=RlAd8FeJNRR8pjrLsuYaK28el919Z3Zm9be1FZXKzv4=";
+
         // Details for support
         public const string DeveloperSupportEmail = "g00309646@gmit.ie";
     }
