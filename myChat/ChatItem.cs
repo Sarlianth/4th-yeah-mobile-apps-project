1a51b435075fc310a181421dd30486c2df56b2a8
Adrian Sypos <adrian00935@gmail.com>
2017-11-23 21:56:00 +0000

    Connected application with Azure Notification Hub and successfully created channel

--- myChat/ChatItem.cs
+++ myChat/ChatItem.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Newtonsoft.Json;
 
-namespace AzureChatr
+namespace myChat
 {
     class ChatItem
     {
--- myChat/MainPage.xaml.cs
+++ myChat/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Networking.PushNotifications;
 using Windows.UI;
 using Windows.UI.Core;
 using Windows.UI.Popups;
@@ -17,11 +19,17 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.WindowsAzure.Messaging;
 
 namespace myChat
 {
     public sealed partial class MainPage : Page
     {
+        private MobileServiceCollection<ChatItem, ChatItem> items;
+        private IMobileServiceTable<ChatItem> chatTable = App.MobileService.GetTable<ChatItem>();
+
+        public PushNotificationChannel PushChannel;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -37,13 +45,55 @@ namespace myChat
             else
             {
                 UpdateStatus("You are connected to the Internet", false);
+                // Channel URI is registered in notification hub
+                InitNotificationsAsync();
             }
         }
 
+        /* Adapted from https://docs.microsoft.com/en-us/azure/notification-hubs/notification-hubs-windows-store-dotnet-get-started-wns-push-notification */
+        private async void InitNotificationsAsync()
+        {
+            Exception exception = null;
+
+            try
+            {
+                var channel = await PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync();
+
+                var hub = new NotificationHub(ConfigSecrets.AzureNotificationHubName, ConfigSecrets.AzureNotificationHubCnxString);
+                var result = await hub.RegisterNativeAsync(channel.Uri);
+
+                // Display id to confirm its successfull
+                if (result.RegistrationId != null)
+                {
+                    var dialog = new MessageDialog("Registration successful: " + result.RegistrationId);
+                    dialog.Commands.Add(new UICommand("OK"));
+                    await dialog.ShowAsync();
+                    // Update status - inform user that the channel is ready
+                    UpdateStatus("Chat channel is ready.", false);
+                }
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            if (exception != null)
+            {
+                UpdateStatus("Could not initialize cloud services to receive messages.", true);
+                string msg1 = "An error has occurred while initializing cloud notifications." + Environment.NewLine + Environment.NewLine;
+
+                string msg2 = "Make sure that you have an active Internet connection and try again.";
+
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    new MessageDialog(msg1 + msg2, "Initialization Error").ShowAsync();
+                });
+            }
+        }
+
+        // Method checkning if device is connected to internet
         private bool CheckForInternetAccess()
         {
-            // This is not the most foolproof way but at least covers basic scenarios
-            // like Airplane mode
             if (NetworkInterface.GetIsNetworkAvailable())
             {
                 return true;
@@ -54,12 +104,14 @@ namespace myChat
             }
         }
 
+        // Method to update textblock with status information in it
         private async Task UpdateStatus(string status, bool isError)
         {
             await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
             {
                 StatusMsg.Text = status;
 
+                // If error, change foreground to red, else black
                 StatusMsg.Foreground = new SolidColorBrush((isError) ? Colors.Red : Colors.Black);
             });
         }
