f272d6b4b5389d4ba5e111850c9b526bdb4795b9
Adrian Sypos <adrian00935@gmail.com>
2017-11-23 00:04:33 +0000

    Check for internet connection, update status messages accordingly

--- myChat/MainPage.xaml.cs
+++ myChat/MainPage.xaml.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -20,6 +25,43 @@ namespace myChat
         public MainPage()
         {
             this.InitializeComponent();
+
+            if (!CheckForInternetAccess())
+            {
+                string msg1 = "An Internet connection is required for this app to work." + Environment.NewLine + Environment.NewLine;
+                string msg2 = "Please, make sure that you have an active Internet connection and try again.";
+                UpdateStatus("You are not connected to the Internet", true);
+
+                new MessageDialog(msg1 + msg2, "No Internet").ShowAsync();
+            }
+            else
+            {
+                UpdateStatus("You are connected to the Internet", false);
+            }
+        }
+
+        private bool CheckForInternetAccess()
+        {
+            // This is not the most foolproof way but at least covers basic scenarios
+            // like Airplane mode
+            if (NetworkInterface.GetIsNetworkAvailable())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private async Task UpdateStatus(string status, bool isError)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                StatusMsg.Text = status;
+
+                StatusMsg.Foreground = new SolidColorBrush((isError) ? Colors.Red : Colors.Black);
+            });
         }
     }
 }
